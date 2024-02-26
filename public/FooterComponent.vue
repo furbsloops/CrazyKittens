196aca2c90768f609d98353d9b93212d10717a46
furbsloops <furbsloops@gmail.com>
2024-02-26 03:37:36 +0100

    first commit

---
+++ public/FooterComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer :style="{ backgroundColor: backgroundColor }">
+    <h1>Crazy Kittens</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+  data() {
+    return {
+      backgroundColor: this.getRandomColor(),
+    };
+  },
+  methods: {
+    getRandomColor() {
+      // Funzione per generare un colore casuale in formato hex
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  padding: 20px;
+  color: white;
+  text-align: center;
+}
+h1 {
+  margin: 0;
+}
+</style>
